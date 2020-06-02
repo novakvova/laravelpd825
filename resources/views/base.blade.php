f20d893b094c6ef24e9e485917c6606fbf3ab7c9
novakvova <novakvova@gmail.com>
2020-06-02 17:40:29 +0300

    Add js froala editot

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Laravel 7 & MySQL CRUD Tutorial</title>
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet" />
 </head>
 <body>
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
@@ -46,6 +46,10 @@
 <div class="container">
     @yield('main')
 </div>
-<script src="{{ asset('js/app.js') }}" type="text/js"></script>
+
+<script src={{ asset('js/app.js') }}></script>
+
+@yield('scripts')
+
 </body>
 </html>
--- resources/views/categories/create.blade.php
+++ resources/views/categories/create.blade.php
@@ -14,7 +14,7 @@
                         </ul>
                     </div><br />
                 @endif
-                <form method="post" action="{{ route('categories.store') }}">
+                <form id="create" method="post" action="{{ route('categories.store') }}">
                     @csrf
                     <div class="form-group">
                         <label for="name">Назва категорії:</label>
@@ -22,14 +22,16 @@
                     </div>
 
                     <div class="form-group">
-                        <label for="last_name">Фото:</label>
-                        <img src="/images/200_default.png" alt="Обрати фото">
-                        <input type="hidden" name="image">
+                        <img class="chose-image" src="{{ asset('images/200_default.png') }}"
+                                id="chooseImage" alt="Обрати фото">
+                        <input type="hidden" name="base64Image" id="base64Image">
+
+                        <input type="file" id="selectImage" class="d-none">
                     </div>
 
                     <div class="form-group">
                         <label for="email">Опис:</label>
-                        <textarea class="form-control" name="description" rows="10" cols="45" ></textarea>
+                        <textarea class="form-control" name="description" id ="edit" rows="10" cols="45" ></textarea>
                     </div>
 
                     <button type="submit" class="btn btn-primary">Add contact</button>
@@ -37,4 +39,120 @@
             </div>
         </div>
     </div>
+
+    <!-- Modal -->
+    <div class="modal" id="cropperModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <div>
+                        <img id="imageCropper" src="{{ asset('images/200_default.png') }}" height="400">
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <a id="img-rotation" class="btn btn-success"><i class="fa fa-repeat" aria-hidden="true"></i></a>
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button id="cropImg" class="btn btn-primary">Обрізати фото</button>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+
+@section('scripts')
+    <script>
+        jQuery(function () {
+            //фото по якому клікаємо і обираємо файл
+            $chooseImage = $("#chooseImage");
+
+            //текстове поле із base64
+            $base64Image = $("#base64Image");
+
+            //скритий інпут для вибору файла
+            $selectImage = $("#selectImage");
+
+
+            let dialogCropper = $("#cropperModal");
+
+            //клікнули по фото і клікаємо по скритому інпуту файл
+            $chooseImage.on("click", function () {
+                $selectImage.click();
+            });
+
+            //коли обрали файл
+            $selectImage.on("change", function() {
+                if (this.files && this.files.length) {
+                    let file = this.files[0];
+                    var reader = new FileReader();
+                    reader.onload = function(e) {
+                        dialogCropper.modal('show');
+                        cropper.replace(e.target.result);
+
+                    }
+                    reader.readAsDataURL(file);
+
+                }
+            });
+
+            //запуск кропера
+            const imageCropper = document.getElementById('imageCropper');
+            const cropper = new Cropper(imageCropper, {
+                aspectRatio: 1/1,
+                viewMode: 1,
+                autoCropArea: 0.5,
+                crop(event) {
+                    // console.log(event.detail.x);
+                    // console.log(event.detail.y);
+                    // console.log(event.detail.width);
+                    // console.log(event.detail.height);
+                    // console.log(event.detail.rotate);
+                    // console.log(event.detail.scaleX);
+                    // console.log(event.detail.scaleY);
+                },
+            });
+
+            //поворот малюнка
+            $("#img-rotation").on("click",function (e) {
+                e.preventDefault();
+                cropper.rotate(45);
+            });
+
+            //обрізка малюнка
+            $("#cropImg").on("click", function (e) {
+                e.preventDefault();
+
+                var imgContent = cropper.getCroppedCanvas().toDataURL();
+                $chooseImage.attr("src", imgContent);
+                dialogCropper.modal('hide');
+                $base64Image.val(imgContent);
+            });
+
+
+        })
+    </script>
+
+    <script>
+        //запускаємо фороалу едітор
+        $(function () {
+            const editorInstance = new FroalaEditor('#edit', {
+                enter: FroalaEditor.ENTER_P,
+                placeholderText: null,
+                events: {
+                    initialized: function () {
+                        const editor = this;
+                        this.el.closest('form').addEventListener('submit', function (e) {
+                            // cosnole.log("--------", editor);
+                            // e.preventDefault();
+                        })
+                    }
+                }
+            })
+        });
+    </script>
 @endsection
