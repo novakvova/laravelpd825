af950268518d4344c4142e434b7b8c71b618aa46
DashaShevchuk <51369257+DashaShevchuk@users.noreply.github.com>
2020-06-18 18:02:17 +0300

    Revert "Dev dan"

--- app/Http/Controllers/Auth/VerificationController.php
+++ app/Http/Controllers/Auth/VerificationController.php
@@ -18,6 +18,7 @@ class VerificationController extends Controller
     | be re-sent if the user didn't receive the original email message.
     |
     */
+
     use VerifiesEmails;
 
     /**
@@ -25,7 +26,6 @@ class VerificationController extends Controller
      *
      * @var string
      */
-
     protected $redirectTo = RouteServiceProvider::HOME;
 
     /**
