f5ac01ec79ac7d25e889e4062023fbfdfee978cf
novakvova <novakvova@gmail.com>
2020-06-20 14:05:20 +0300

    Good work project

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -28,7 +28,9 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
  * for events that are broadcast by Laravel. Echo and event broadcasting
  * allows your team to easily build robust real-time web applications.
  */
+window.Cropper = require('cropperjs');
 
+window.xzoom = require('./xzoom.min');
 // import Echo from 'laravel-echo';
 
 // window.Pusher = require('pusher-js');
